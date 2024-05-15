311db99de3230a3005dc00ab5350ed5a691a9175
clement-vitrat <clement.vitrat@campus.academy>
2024-05-15 12:12:18 +0200

    test

--- src/components/Accueil/bienvenue.tsx
+++ src/components/Accueil/bienvenue.tsx
@@ -18,7 +18,6 @@ export default function Bienvenue() {
           Santé
         </h1>
 
-<<<<<<< Updated upstream
         <div className={token.identity ? 'container' : 'container content'}>
           {token.identity != 'isuser' && (
             <div className="present aos-init aos-animate" data-aos="fade-right">
@@ -59,44 +58,6 @@ export default function Bienvenue() {
               </Link>
             </div>
           )}
-=======
-        <div className="container content">
-          <div className="present aos-init aos-animate" data-aos="fade-right">
-            <h4>Utilisateur</h4>
-            <p>
-              En tant qu'utilisateur, vous pourrez interargir avec des
-              associations et des utilisateurs.
-            </p>
-            <p>Vous n'avez pas encore de compte</p>
-            <p className="cliquez">Cliquez ci-dessous.</p>
-            <Link href="InscrireUser" className="button">
-              Créer un compte
-            </Link>
-            <p>Vous possédez un compte</p>
-            <p className="cliquez">Cliquez ci-dessous.</p>
-            <Link href="ConnecterUser" className="button">
-              Se connecter
-            </Link>
-          </div>
-
-          <div className="present aos-init aos-animate" data-aos="fade-right">
-            <h4>Association</h4>
-            <p>
-              En tant qu'association, vous pourriez faire découvrir votre
-              association et ses ressources.
-            </p>
-            <p>Qu'attendez-vous pour ajouter votre association</p>
-            <p className="cliquez">Cliquez ci-dessous.</p>
-            <Link href="InscrireAsso" className="button">
-              Ajouter votre association
-            </Link>
-            <p>Vous possédez un compte</p>
-            <p className="cliquez">Cliquez ci-dessous.</p>
-            <Link href="ConnecterAsso" className="button">
-              Se connecter
-            </Link>
-          </div>
->>>>>>> Stashed changes
         </div>
       </section>
     </>
